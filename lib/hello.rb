fed7de9c4de51595c9eeadbc81a794320f7051e2
beatific-revision-5216 <ivanov_chris@yahoo.com>
2019-05-29 02:36:01 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,5 +1,11 @@
-def hello_t
-
+def hello_t(names)
+  if block_given?
+    for name in names do
+      yield(name)
+    end
+  else
+    puts "Hey! No block was given!"
+  end
 end
 
 # call your method here!
